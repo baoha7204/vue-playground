bf496f6b915bef28e347debe74ddedcdb120ec5e
baoha7204 <baoha3604@gmail.com>
2024-09-10 18:32:28 +0700

    [BaoHMQ] basic dark mode setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useDark, useDateFormat, useNow, useToggle } from "@vueuse/core";
+
+const formatted = useDateFormat(useNow(), "MM-DD-YYYY HH:mm:ss");
+
+const isDark = useDark({
+  disableTransition: false,
+});
+
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <div
+    class="bg-white text-slate-800 dark:bg-slate-800 dark:text-white flex flex-col items-center m-10 gap-10"
+  >
+    <button class="bg-green-300 p-2 font-semibold" @click="toggleDark()">
+      Switch mode
+    </button>
+    <h1 class="text-3xl">{{ formatted }}</h1>
+  </div>
+</template>
